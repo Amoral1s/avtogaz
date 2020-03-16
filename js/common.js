1539529f3e49bfd5a9e549869d71cc4d08010324
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-03-16 17:58:07 +0300

    2

--- js/common.js
+++ js/common.js
@@ -1,12 +1,50 @@
 jQuery(document).ready(function () {
 
-  jQuery('.work-wrap').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    infinite: true,
-    dots: true,
-    arrows: true,
-    autoplay: false
-  });
+  if ($(window).width() <= 768) {
+
+
+    jQuery('.work-wrap').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: true,
+      autoplay: false
+    });
+    jQuery('.features-wrap').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: true,
+      autoplay: false
+    });
+
+    $('.mob-toggle').on('click', function() {
+      $(this).toggleClass('toggle-active');
+      $('.header-bottom').slideToggle(500);
+      $('.header-top').slideToggle(500);
+
+    });
+
+  } else if ($(window).width() <= 992) {
+    jQuery('.work-wrap').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: true,
+      autoplay: false
+    });
+  } else {
+    jQuery('.work-wrap').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+      arrows: true,
+      autoplay: false
+    });
+  }
   
 });
